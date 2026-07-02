64c00052eb57fb4a653e4ac16db4d25e337f5479
OwinoMichael <owinomichael1400@gmail.com>
2026-07-02 14:44:46 -0400

    open dir, read dir loop, and close dir

--- ls.c
+++ ls.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
     const char *path = (argc > 1) ? argv[1] : ".";
 
-    printf("%s\n", path);
+    DIR *dir = opendir(path);
+    if(!dir){
+        perror("opendir");
+        return 1;
+    }
+
+    struct dirent *entry;
+    while((entry = readdir(dir)) != NULL){
+        if(entry->d_name == '.') continue;
+        printf("%s\n", entry->d_name);
+    }
+
+    closedir(dir);
+    
+
+    // printf("%s\n", path);
     return 0;
 }
